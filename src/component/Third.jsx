25650aea848cdb1b90ec6c22ef8cd3b86e124971
software-paraminnovation <software@paraminnovation.org>
2025-10-11 09:19:33 +0530

    added

--- src/component/Third.jsx
+++ src/component/Third.jsx
@@ -1,31 +1,32 @@
-// Third.jsx
-import React, { useState, useEffect, useRef } from "react";
+
+
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { setAnswer } from "../features/survey/surveySlice";
 
-
 function Third({ onNext, onHome }) {
   const dispatch = useDispatch();
+  const [selected, setSelected] = useState("");
 
   const options = [
-    "Google Search",
-    "Word of Mouth",
-    "Social Media",
-    "School",
-    "Other",
+    "Friends/Family/Word of mouth",
+    "Newspaper/Poster/Advertisement/Coupon",
+    "Param events or stalls",
+    "Social Media/Instagram",
   ];
 
   const pick = (label) => {
+    setSelected(label);
     dispatch(setAnswer({ questionIndex: 1, answer: label }));
     onNext?.();
   };
 
- 
   return (
     <div className="flex flex-col min-h-full w-full bg-[url('/background.svg')] bg-cover bg-no-repeat items-center relative overflow-hidden text-black">
-      <div className="flex flex-col items-start w-full px-28 py-28 gap-20 relative">
+      <div className="flex flex-col items-start w-full px-16 lg:px-28 py-20 lg:py-28 gap-20 relative">
+        
         {/* Home button */}
-        <div className="absolute top-0 right-0 px-22 py-15" onClick={onHome}>
+        <div className="absolute top-0 right-0 px-6 py-6" onClick={onHome}>
           <img
             src="/home_button.svg"
             alt="home"
@@ -34,54 +35,28 @@ function Third({ onNext, onHome }) {
         </div>
 
         {/* Question */}
-        <div className="leading-[100%] h-[40%] font-oxanium text-[3rem] md:text-[4rem] lg:text-[5rem] text-justify flex flex-col gap-1.5">
-          <div>How did you find out </div>
-          <div>about PARSEC </div>
+        <div className="leading-[100%] font-oxanium text-[2.5rem] md:text-[3.5rem] lg:text-[4.5rem] text-left flex flex-col gap-2">
+          <div>How did you find out</div>
+          <div>about PARSEC</div>
           <div>Jayanagar?</div>
         </div>
 
-        {/* Buttons */}
-        <div className="flex flex-col h-[60%] w-full gap-16 font-inter-custom">
-          <div className="flex gap-3.5 justify-between w-full">
+        {/* Options */}
+        <div className="flex flex-col w-full gap-8 font-inter-custom">
+          {options.map((opt, i) => (
             <button
-              onClick={() => pick(options[0])}
-              className="py-10 px-10 w-[50%] text-4xl font-medium text-gray-900 bg-white rounded-full border border-black/20 hover:bg-gray-100"
+              key={i}
+              onClick={() => pick(opt)}
+              className={`py-8 px-10 text-left text-2xl lg:text-3xl font-medium rounded-full border border-black/20 transition-all duration-200
+                ${
+                  selected === opt
+                    ? "bg-gray-200 border-gray-500 scale-[1.02]"
+                    : "bg-white hover:bg-gray-100"
+                }`}
             >
-              {options[0]}
+              {opt}
             </button>
-
-            <button
-              onClick={() => pick(options[1])}
-              className="py-10 px-10 w-[50%] text-4xl font-medium text-gray-900 bg-white rounded-full border border-black/20 hover:bg-gray-100"
-            >
-              {options[1]}
-            </button>
-          </div>
-
-          <div className="flex gap-3.5 justify-between w-full">
-            <button
-              onClick={() => pick(options[2])}
-              className="py-10 px-10 w-[50%] text-4xl font-medium text-gray-900 bg-white rounded-full border border-black/20 hover:bg-gray-100"
-            >
-              {options[2]}
-            </button>
-
-            <button
-              onClick={() => pick(options[3])}
-              className="py-10 px-10 w-[50%] text-4xl font-medium text-gray-900 bg-white rounded-full border border-black/20 hover:bg-gray-100"
-            >
-              {options[3]}
-            </button>
-          </div>
-
-          <div className="flex gap-3.5 justify-between w-full">
-            <button
-              onClick={() => pick(options[4])}
-              className="py-10 px-10 w-[50%] text-4xl font-medium text-gray-900 bg-white rounded-full border border-black/20 hover:bg-gray-100"
-            >
-              {options[4]}
-            </button>
-          </div>
+          ))}
         </div>
       </div>
 
@@ -89,8 +64,6 @@ function Third({ onNext, onHome }) {
       <div className="absolute left-0 -bottom-[2%] right-1/4">
         <img src="/astrothirdpage.png" alt="astronaut" className="w-[100rem]" />
       </div>
-
-  
     </div>
   );
 }
